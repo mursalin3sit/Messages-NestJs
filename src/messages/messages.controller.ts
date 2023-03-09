d62934082962234b418903c1a63a6a1e239700d5
Mursalin <mursalin3sit@gmail.com>
2023-03-09 12:01:51 +0600

    Update

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Param, NotFoundException, } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+   constructor(public messagesService: MessagesService) {}
+
+  @Get()
+   listMessages() {
+      return this.messagesService.findAll();
+   }
+
+   @Post()
+   createMessages(@Body() body: CreateMessageDto) {
+      return this.messagesService.create(body.content);
+   }
+
+   @Get('/:id')
+   async getMessages(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('messages not found');
+    }
+
+    return message;
+   }
+}
